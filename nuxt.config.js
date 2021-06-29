277d19113f5b62cace4cb4dd938bf52b980cc754
daletom <tom@imgix.com>
2021-06-28 22:38:54 -0700

    remove lato

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,10 +17,6 @@ export default {
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
-      {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css?family=Lato:300,400,700,900"
-      }
     ]
   },
 
