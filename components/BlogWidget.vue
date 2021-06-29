7e07d192fc667f97be3e15789c0a5ef816a81b84
daletom <tom@imgix.com>
2021-06-28 22:22:43 -0700

    with ads

--- components/BlogWidget.vue
+++ components/BlogWidget.vue
@@ -1,16 +1,14 @@
 <template>
-    <div class="flex flex-wrap">
+    <div>
         <!-- Template for blog posts -->
-      <div v-for="post in posts" :key="post.id" v-bind:post="post" class="p-4 lg:w-1/3 md:w-1/2 sm:w-full">
-        <h2>{{ $prismic.asText(post.data.title) }}</h2>
+      <h2>{{ $prismic.asText(post.data.title) }}</h2>
         <ix-img
-        :src="post.data.heroimage.url"
+        :src="imageSrc(post)"
         :imgixParams="{ fit: 'crop', ar: '1.8:1', crop: 'faces,edges', ch: 'dpr,width' }"
         sizes="(min-width: 1024px) calc(33vw - 60px), (min-width: 768px) calc(50vw - 100px), calc(100vw - 70px)"
         loading="lazy"
         />
         <p>{{ $prismic.asText(post.data.content) }}</p>
-      </div>
     </div>
 </template>
 
@@ -30,17 +28,9 @@ export default {
   },
   name: 'blog-widget',
   methods: {
-    imageSrc() {
-      return (new URL(this.post.data.heroimage.url)).pathname
+    imageSrc(post) {
+      return (new URL(post.data.heroimage.url)).pathname
     }
   }
 }
 </script>
-
-<style lang="sass" scoped>
-.blog-post
-  color: #353535
-
-h2
-  margin: 0
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="p-4">
+  <div class="lg:pl-24 lg:pr-24 pt-4">
     <Hero />
-      <!-- <div class="blog-avatar" :style="{ backgroundImage: 'url(' + image + ')' }" ></div>
-      <h1 class="blog-title">
-        {{ $prismic.asText(homepageContent.headline) }}
-      </h1>
-      <p class="blog-description">{{ $prismic.asText(homepageContent.description) }}</p> -->
-      
+      <!-- <div class="blog-avatar" :style="{ backgroundImage: 'url(' + image + ')' }" ></div>-->
+      <div class="text-center">
+        <h1 class="blog-title">
+           {{ $prismic.asText(teaser.title) }}
+          </h1>
+      <div class="py-8 px-8 mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 md:py-4 md:flex md:items-center md:space-y-0 md:space-x-6">
+        <ix-img
+        class="lg:w-3/5 md:w-1/3 w-full"
+        :src="teaserSrc(teaser)"
+        :imgixParams="{ fit: 'crop', ar: '1.8:1', crop: 'faces,edges', ch: 'dpr,width' }"
+        loading="lazy"
+        sizes="(min-width: 1024px) calc(60vw - 192px), 75vw"
+        />
+        <div class="text-center space-y-2">
+          <div class="space-y-0.5">
+          <p class="blog-description inline">{{ $prismic.asText(teaser.content) }}</p> 
+          </div>
+        </div>
+      </div>
+      </div>
       <!-- Check blog posts exist -->
     <div v-if="posts.length !== 0" class="flex flex-wrap">
         <!-- Template for blog posts -->
@@ -19,6 +33,10 @@
         loading="lazy"
         />
         <p>{{ $prismic.asText(post.data.content) }}</p>
+      </div>
+      <div class="p-4 pt-16 lg:w-1/3 md:w-1/2 sm:w-full">
+      <video loading="lazy" muted playsinline autoplay src="https://storage.googleapis.com/adforum-media/34489704/ad_34489704_557e58100e2d3f8a_web.mp4"></video>
+      <p>Mock Video Ad from Nissan</p>
       </div>
     </div>
       <!-- If no blog posts return message -->
@@ -44,7 +62,7 @@ export default {
     try{
       // Query to get blog home content
       //const homepageContent = (await $prismic.api.getSingle('blog_home')).data
-
+      const teaser = (await $prismic.api.getSingle('teaser')).data
       // Query to get posts content to preview
       const blogPosts = await $prismic.api.query(
         $prismic.predicates.at("document.type", "post")
@@ -53,7 +71,7 @@ export default {
 
       // Returns data to be used in template
       return {
-        //homepageContent,
+        teaser,
         posts: blogPosts.results,
         //image: homepageContent.image.url,
       }
@@ -65,6 +83,9 @@ export default {
   methods: {
     imageSrc(post) {
       return (new URL(post.data.heroimage.url)).pathname
+    },
+     teaserSrc(teaser) {
+      return (new URL(teaser.heroimage.url)).pathname
     }
   }
 }
